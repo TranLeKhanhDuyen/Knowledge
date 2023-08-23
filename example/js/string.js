80864747e683f61c2c387f4127275fc81c6648fc
Duyen <trankhanhduyen312@gmail.com>
2023-08-23 16:51:34 +0700

    feat: add code example

--- example/js/string.js
+++ example/js/string.js
@@ -3,9 +3,7 @@ console.log(a);
 console.log(4 + 5 + "a"); // = 9 + 'a' = 9a
 console.log("a" + 5 + 4); // = a5 + 4 = a54
 
-// var fistName = "Duyen";
-// var lastName = "Tran";
-// console.log('Toi la: ${fistname} ${lastName}')
 var firstName = "Duyen";
 var lastName = "Tran";
 console.log(`Toi la: ${firstName} ${lastName}`);
+console.log(lastName.charAt(0));

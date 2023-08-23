cfd1f20be181fb143535880da83702d8dcbf1fc2
Duyen <trankhanhduyen312@gmail.com>
2023-08-23 16:43:02 +0700

    feat: add code example

---
+++ example/js/main.js
@@ -0,0 +1,13 @@
+var weight = 56;
+console.log(weight);
+
+//Operations
+var age = 22;
+var nextAge = age + 1;
+console.log(age);
+
+//boolean
+var a = 1;
+var b = 2;
+var isSucces = a < b;
+console.log(isSucces); //true

762ce23f38ed2d1ecb04c42920434f20fd41acc1
TranLeKhanhDuyen <117955743+TranLeKhanhDuyen@users.noreply.github.com>
2023-08-23 16:44:57 +0700

    feat: update code example

--- example/js/string.js
+++ example/js/string.js
@@ -3,9 +3,6 @@ console.log(a);
 console.log(4 + 5 + "a"); // = 9 + 'a' = 9a
 console.log("a" + 5 + 4); // = a5 + 4 = a54
 
-// var fistName = "Duyen";
-// var lastName = "Tran";
-// console.log('Toi la: ${fistname} ${lastName}')
 var firstName = "Duyen";
 var lastName = "Tran";
 console.log(`Toi la: ${firstName} ${lastName}`);

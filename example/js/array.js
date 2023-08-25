65e77408c22028ed6df5a9943a5805af1aef06c2
Duyen <trankhanhduyen312@gmail.com>
2023-08-25 16:12:49 +0700

    feat: add code example with array method

--- example/js/array.js
+++ example/js/array.js
@@ -26,7 +26,6 @@ var cars = ["Honda", "Mazda", "Mercedes"];
 var result = joinWithCharacter(cars, " -- ");
 console.log(result);
 
-
 function getLastElement(asc) {
   return asc.slice(-1);
 }
@@ -34,3 +33,104 @@ var animals = ["Monkey", "Tiger", "Elephant"];
 var result = getLastElement(animals);
 console.log(result); // Output: "Elephant"
 console.log(animals); // Output: ['Monkey', 'Tiger', 'Elephant']
+
+// ===  ARRAY METHODS ===
+var courses = [
+  {
+    id: 1,
+    name: "JS",
+    coin: 0,
+  },
+  {
+    id: 2,
+    name: "HTML,CSS",
+    coin: 200,
+  },
+  {
+    id: 3,
+    name: "React",
+    coin: 0,
+  },
+  {
+    id: 4,
+    name: "Python",
+    coin: 100,
+  },
+];
+
+// =========== forEach ====================
+courses.forEach(function (courses, index) {
+  console.log(index, courses);
+});
+
+// =========== every ====================
+
+var isFree = courses.every(function (courses, index) {
+  console.log(index, courses);
+  return courses.coin === 0;
+});
+console.log(isFree);
+
+// =========== find =============
+var course = courses.find(function (courses, index) {
+  console.log(index, courses);
+  return courses.name === "JS";
+});
+console.log(course);
+
+// ============= map ==============
+var newCourses = courses.map(function (course) {
+  return {
+    id: course.id,
+    name: `Language: ${course.name}`,
+    coin: course.coin,
+    coinText: `Price: ${course.coin}`,
+  };
+});
+console.log(newCourses);
+
+//=========== reduce ===========
+function coinHandler(accumulator, currentValue) {
+  return accumulator + currentValue.coin;
+}
+var totalCoin = courses.reduce(coinHandler, 0);
+console.log(totalCoin);
+
+//ex2: reduce
+const numbers = [45, 4, 9, 16, 25];
+let sum = numbers.reduce(myFunction);
+console.log("The sum is " + sum);
+
+function myFunction(total, value, index, array) {
+  return total + value;
+}
+
+//========== filter ============
+const sports = [
+  {
+    name: "volleyball",
+    like: 6,
+  },
+  {
+    name: "badmintion",
+    like: 5,
+  },
+  {
+    name: "boxing",
+    like: 2,
+  },
+  {
+    name: "climbing",
+    like: 4,
+  },
+  {
+    name: "cycling",
+    like: 7,
+  },
+];
+function getMostFavoriteSport(sports) {
+  return sports.filter(function (sport) {
+    return sport.like >= 5;
+  });
+}
+console.log(getMostFavoriteSport(sports));

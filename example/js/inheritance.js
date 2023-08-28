c30d1ff3f932121c5692f91d4fb1ecee4a5459cd
Duyen Tran <trankhanhduyen312@gmail.com>
2023-08-28 17:18:02 +0700

    feat: add code example with inheritance

--- example/js/inheritance.js
+++ example/js/inheritance.js
@@ -1,22 +1,50 @@
 class Car {
-    constructor(brand) {
-      this.carname = brand;
-    }
-    present() {
-      return 'I have a ' + this.carname;
-    }
+  constructor(brand) {
+    this.carname = brand;
   }
-  
-  class Model extends Car {
-    constructor(brand, mod) {
-      super(brand);
-      this.model = mod;
-    }
-    show() {
-      return this.present() + ', it is a ' + this.model;
-    }
+  present() {
+    return "I have a " + this.carname;
   }
-  
-  let myCar = new Model("Ford", "Mustang");
-  console.log(myCar.show()); 
-  
+}
+
+class Model extends Car {
+  constructor(brand, mod) {
+    super(brand);
+    this.model = mod;
+  }
+  show() {
+    return this.present() + ", it is a " + this.model;
+  }
+}
+
+let myCar = new Model("Ford", "Mustang");
+console.log(myCar.show());
+
+//=== Getters & setters
+class Person {
+  constructor(firstName, lastName) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  // Getter for the full name
+  get fullName() {
+    return this._firstName + " " + this._lastName;
+  }
+
+  // Setter for the full name
+  set fullName(name) {
+    const parts = name.split(" ");
+    this._firstName = parts[0];
+    this._lastName = parts[1];
+  }
+}
+
+const person = new Person("John", "Doe");
+
+console.log(person.fullName); // John Doe
+
+person.fullName = "Jane Smith";
+console.log(person.fullName); // Jane Smith
+
+console.log(person._firstName); // John (still accessible but conventionally considered private)

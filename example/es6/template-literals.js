f70f7b2f8637dd6b97c0663f749a50eb7476fa52
Duyen Tran <trankhanhduyen312@gmail.com>
2023-08-31 09:44:41 +0700

    feat: add code example with template string

---
+++ example/es6/template-literals.js
@@ -0,0 +1,15 @@
+// === template string ===
+
+const courseName = "Javascript";
+// const description = "Course Name: " + courseName;  // use + to concatenate strings
+// console.log(description);
+
+const description = `Course name: ${courseName}`; // use template string
+console.log(description);
+
+// ===  multi-line string ===
+const lines = `Line 1
+Line 2
+line 3
+line 4`;
+console.log(lines);

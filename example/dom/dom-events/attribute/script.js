45633ae0ae95951b065e1b525eed1063f5f376fa
Duyen Tran <trankhanhduyen312@gmail.com>
2023-09-11 14:52:11 +0700

    feat: add code example with dom attributes

--- example/dom/dom-events/attribute/script.js
+++ example/dom/dom-events/attribute/script.js
@@ -33,3 +33,9 @@ document.onkeydown = function (e) {
       break;
   }
 };
+
+// Function to modify the text content of the paragraph
+const changeText = () => {
+  const p = document.querySelector("p");
+  p.textContent = "I changed because of an inline event handler.";
+};

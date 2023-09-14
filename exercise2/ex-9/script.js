89aaee1596c35b2fa03166c86dd22d887aead207
Duyen <trankhanhduyen312@gmail.com>
2023-09-14 15:33:47 +0700

    feat: count and display items of a dropdown list

--- exercise2/ex-9/script.js
+++ exercise2/ex-9/script.js
@@ -1,27 +1,18 @@
-// function getOptions() {
-//     const select = document.getElementById("mySelect");
-//     const options = select.options;
-//     const optionCount = options.length;
+//solution  1
+function getOptions() {
+    const select = document.getElementById("mySelect");
+    const options = select.options;
+    const optionCount = options.length;
 
-//     let message = "Các mục trong danh sách thả xuống:\n";
-//     for (let i = 0; i < optionCount; i++) {
-//       message += options[i].text + "\n";
-//     }
+    let message = "Các mục trong danh sách thả xuống:\n";
+    for (let i = 0; i < optionCount; i++) {
+      message += options[i].text + "\n";
+    }
 
-//     alert("Số lượng mục: " + optionCount + "\n" + message);
-//   }
+    alert("Số lượng mục: " + optionCount + "\n" + message);
+  }
 
-// getOptions = () => {
-//     const select = document.getElementById('mySelect');
-//     const options = Array.from(select.options);
-//     let items = "Dropdown list items:\n"
-
-//     options.forEach((option) => {
-//         items += option.textContent + "\n";
-//     });
-//     alert(items + "And length of list items is: " + options.length);
-// }'
-
+//solution 2
 getOptions = () => {
   const select = document.getElementById("mySelect");
   const options = Array.from(select.options);

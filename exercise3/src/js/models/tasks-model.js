a54dc3250251d64c6690cbff60414999aad863d7
Duyen Tran <trankhanhduyen312@gmail.com>
2023-10-02 16:55:02 +0700

    feat: create task model

---
+++ exercise3/src/js/models/tasks-model.js
@@ -0,0 +1,29 @@
+export default class TaskModel {
+    constructor() {
+        this.tasks = [
+            { id: 1, text: "abc", complete: false },
+            { id: 2, text: "cde", complete: false },
+        ];
+    }
+
+    addTask(taskText) {
+        const task = {
+            id:
+                this.tasks.length > 0
+                    ? this.tasks[this.tasks.length - 1].id + 1
+                    : 1,
+            text: taskText,
+            complete: false,
+        };
+
+        this.tasks.push(task);
+    }
+
+    editTask(id, updateTask) {
+        this.tasks = this.tasks.map((task) =>
+            task.id === id
+                ? { id: task.id, text: updateTask, complete: task.complete }
+                : task,
+        );
+    }
+}

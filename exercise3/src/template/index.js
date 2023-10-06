cd515d1c409685ce5dbea1275fae748f0fc739d2
Duyen Tran <trankhanhduyen312@gmail.com>
2023-10-06 15:34:14 +0700

    feat: update template for task

--- exercise3/src/template/index.js
+++ exercise3/src/template/index.js
@@ -1,5 +1,6 @@
+import editIcon from "../assets/icons/icon-option.png";
 
-class taskTemplate {
+export default class taskTemplate {
     constructor() {}
 
     static renderTask = (newTask) =>
@@ -9,5 +10,16 @@ class taskTemplate {
               </span>
               <p class="task-name">${newTask.taskName}</p>
             </div>
+            <div class="task-wrapper">
+              <div class="task-item-done">
+              <span class="task-actual">0</span>
+        
+              <span class="task-est">${newTask.est}</span>
+            </div>
+             <button class="btn-optio">
+              <img src="${editIcon}" alt="icon more" class="icon-more edit">
+        </button>
+      </div>
+    </li>
     `;
 }

1b9de2185405c3ee9a743c5ae78be6f4ce8cf1a8
Duyen Tran <trankhanhduyen312@gmail.com>
2023-10-06 16:22:16 +0700

    feat: add task list

---
+++ exercise3/src/js/models/task-list-model.js
@@ -0,0 +1,34 @@
+export default class TaskListModel {
+    constructor() {
+        this.tasks = [];
+    }
+
+    addTask(taskName, est) {
+        const task = {
+            id: Date.now(),
+            taskName,
+            est,
+            isCompleted: false,
+        };
+
+        this.tasks.pusk(task);
+    }
+
+    updateTask(updateTask) {
+        const taskIndex = this.tasks.findIndex(
+            (task) => task.id === updatedTask.id
+        );
+
+        if (taskIndex !== -1) {
+            this.tasks[tasksIndex] = { ...this.task[taskIndex], ...updateTask };
+        }
+    }
+
+    deleteTask(taskId) {
+        const taskId = this.tasks.findIndex((task) => task.id === taskId);
+
+        if (taskIndex !== -1) {
+            this.tasks.splice(taskIndex, 1);
+        }
+    }
+}

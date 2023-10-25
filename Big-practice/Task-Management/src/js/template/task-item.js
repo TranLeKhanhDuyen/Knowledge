4bb253695ca2779719723a1ef62f1a95fff5b406
Duyen <trankhanhduyen312@gmail.com>
2023-10-25 17:30:03 +0700

    feat: create task item template

---
+++ Big-practice/Task-Management/src/js/template/task-item.js
@@ -0,0 +1,11 @@
+export default class TaskItem {
+  /**
+   *  HTML template for rendering the task item.
+   */
+  constructor() {}
+  static renderTaskItem = () => {
+    return `
+
+    `;
+  };
+}

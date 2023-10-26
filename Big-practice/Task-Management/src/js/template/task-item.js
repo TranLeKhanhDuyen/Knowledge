daa0a9d8e8bce1bbaa6b83a15a7a71bd4ba2b055
Duyen <trankhanhduyen312@gmail.com>
2023-10-26 14:55:59 +0700

    feat: add JSDoc

--- Big-practice/Task-Management/src/js/template/task-item.js
+++ Big-practice/Task-Management/src/js/template/task-item.js
@@ -2,10 +2,17 @@ import iconDelete from "../../assets/icons/delete.svg";
 import iconClock from "../../assets/icons/clock.svg";
 
 export default class TaskItem {
-  /**
-   *  HTML template for rendering the task item.
-   */
   constructor() {}
+
+  /**
+   * Create an HTML template string to display a task item.
+   *
+   * @param {Object} data - Information about the task item to be displayed.
+   * @param {string} data.task.title - The title of the task.
+   * @param {string} data.task.createDate - The task's creation date.
+   * @param {string} data.task.dueDate - The task's due date.
+   */
+
   static renderTaskItem = (data) => {
     return `
     <div class="task-item-container">

2012a86b909dce35e05c6a886601c4d34bf4b5f0
Duyen <trankhanhduyen312@gmail.com>
2023-10-26 14:52:24 +0700

    feat: create task item template

--- Big-practice/Task-Management/src/js/template/task-item.js
+++ Big-practice/Task-Management/src/js/template/task-item.js
@@ -1,11 +1,25 @@
+import iconDelete from "../../assets/icons/delete.svg";
+import iconClock from "../../assets/icons/clock.svg";
+
 export default class TaskItem {
   /**
    *  HTML template for rendering the task item.
    */
   constructor() {}
-  static renderTaskItem = () => {
+  static renderTaskItem = (data) => {
     return `
-
-    `;
+    <div class="task-item-container">
+    <div class="task-content">
+      <h3 class="task-title">${data.task.title}</h3>
+      <img src="${iconDelete}" alt="delete icon" class="icon-delete">
+    </div>
+    <div class="task-date">
+      <p class="date-ago">${data.task.createDate}</p>
+      <div class="date-left">
+        <img src="${iconClock}" class="clock-icon"></img>
+        <p>${data.task.dueDate}</p>
+      </div>
+    </div>
+  </div>`;
   };
 }

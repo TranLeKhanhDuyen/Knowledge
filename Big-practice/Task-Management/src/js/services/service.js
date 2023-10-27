e6669df0e07303d40919acd4252bf55cc09f3c90
Duyen <trankhanhduyen312@gmail.com>
2023-10-27 16:54:45 +0700

    feat: update service

--- Big-practice/Task-Management/src/js/services/service.js
+++ Big-practice/Task-Management/src/js/services/service.js
@@ -1,35 +1,51 @@
 import { API_URL } from "../constants/url";
+
+/**
+ * A class for making API requests.
+ */
 export default class APIService {
   /**
-   * Create an instance of the APIService.
-   * @param {string} path - The base API path.
+   * Creates an instance of APIService.
+   * @param {string} path - The API endpoint path.
    */
-
   constructor(path) {
     this.apiUrl = API_URL;
     this.path = path;
   }
 
   /**
-   * Get a list of items from the API.
-   * @returns {Promise} A promise that resolves with the API response.
+   * Sends a GET request to retrieve a list of items.
+   * @returns {Promise} A promise that resolves to the response data.
    */
   getList = () => this.sendRequest(null, "get");
 
   /**
-   * Get the details of an item from the API.
-   * @param {string} id - The ID of the item to retrieve.
-   * @returns {Promise} A promise that resolves with the API response.
+   * Sends a GET request to retrieve the description of a specific item.
+   * @param {number} id - The ID of the item to retrieve.
+   * @returns {Promise} A promise that resolves to the response data.
    */
   getDescription = (id) => this.sendRequest(id, "get");
 
   /**
-   * Send an HTTP request to the API.
-   * @param {string|null} id - The ID of the item for detail requests, or null for list requests.
-   * @param {string} method - The HTTP method (e.g., "get", "post").
-   * @returns {Promise} A promise that resolves with the API response.
+   * Sends a HTTP request to the API.
+   * @param {number | null} id - The ID of the item (if applicable).
+   * @param {string} method - The HTTP method for the request (e.g., "GET", "POST").
+   * @param {*} body - The request payload (if applicable).
+   * @returns {Promise} A promise that resolves to the response data.
    */
   sendRequest = async (id, method, body) => {
-    
-  }
+    const url = `${this.apiUrl}/${this.path}/${id}`;
+    const response = await fetch(url, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error("Error while sending request");
+    }
+  };
 }

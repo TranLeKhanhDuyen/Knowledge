116eb6045f412f06df4cbc2e73de22f43e7bf3a6
unknown <trankhanhduyen312@gmail.com>
2023-10-30 15:53:51 +0700

    feat: update component

--- Big-practice/Task-Management/src/js/template/task-item.js
+++ Big-practice/Task-Management/src/js/template/task-item.js
@@ -16,11 +16,11 @@ export default class TaskItem {
   static renderTaskItem = (data) => {
     return `
     <div class="task-item-container">
-    <div class="task-content">
+    <div class="task-content text-md">
       <h3 class="task-title">${data.task.title}</h3>
       <img src="${iconDelete}" alt="delete icon" class="icon-delete">
     </div>
-    <div class="task-date">
+    <div class="task-date text-md">
       <p class="date-ago">${data.task.createDate}</p>
       <div class="date-left text-sm">
         <img src="${iconClock}" class="clock-icon">

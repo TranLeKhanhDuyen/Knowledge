ae1c994312e6be24079bdf6a781cd88b990c056e
Duyen <trankhanhduyen312@gmail.com>
2023-11-01 09:01:27 +0700

    feat: create meassage

---
+++ Big-practice/Task-Management/src/js/constants/message.js
@@ -0,0 +1,15 @@
+/**
+ * Message for alert the error while fetching or rendering.
+ */
+export const ERROR_MESSAGE = {
+  LOAD_ERROR: "Error when loading data",
+  SERVER_ERROR: "Server Error! Please try again later",
+};
+
+/**
+ * Message for alert if the action be done successfully.
+ */
+export const SUCCESS_MESSAGE = {
+  ADD_TASK: "Add task successfully",
+  DELETE_TASK: "Delete task successfully",
+};

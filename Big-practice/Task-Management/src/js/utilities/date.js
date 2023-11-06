27987789353021d40b0444a01f5467deca577814
Duyen <trankhanhduyen312@gmail.com>
2023-11-06 09:53:20 +0700

    feat: update date

--- Big-practice/Task-Management/src/js/utilities/date.js
+++ Big-practice/Task-Management/src/js/utilities/date.js
@@ -9,21 +9,14 @@ const getDueDate = () => {
   return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
 };
 
-const currentDate = getCurrentDate();
-const dueDate = getDueDate();
-
-console.log(`Current Date: ${currentDate}`);
-console.log(`Due Date: ${dueDate}`);
-
-
-function diffTime(date, method = Math.floor, adverb = 'ago') {
+const diffTime = (date, method = Math.floor, adverb = "ago") => {
   const convertDate = new Date(date);
   const day = 1000 * 60 * 60 * 24;
   const diff = method(Math.abs(convertDate - Date.now()) / day);
 
   switch (diff) {
     case 0:
-      return 'Today';
+      return "Today";
 
     case 1:
       return `${diff} day ${adverb}`;
@@ -35,52 +28,10 @@ function diffTime(date, method = Math.floor, adverb = 'ago') {
     default:
       return date;
   }
-}
+};
 
-
-function convertDateInput(dateStr) {
-  const [month, day, year] = dateStr.split('/');
-  const date = new Date(+year, +month - 1, +day + 1);
-  const [dateValue] = date.toISOString().split('T');
-
-  return dateValue;
-}
-
-function formatDate(date) {
-  const [year, month, day] = date.split('-');
-
-  return [month, day, year].join('/');
-}
-
-function timeElapse(timeStamp) {
-  const minutes = 1000 * 60;
-  const diffMins = Math.floor((Date.now() - (+timeStamp)) / minutes);
-  const [dateString] = new Date(timeStamp).toISOString().split('T');
-  const diffHours = Math.floor(diffMins / 60);
-
-  switch (true) {
-    case (diffMins === 0):
-      return 'Just now';
-    
-    case (diffMins === 1): 
-      return `${diffMins} min ago`;
-
-    case (diffMins < 60):
-      return `${diffMins} mins ago`;
-
-    case (diffMins < (60 * 24)):
-      
-      return `${diffHours} ${(diffHours === 1) ? 'hour' : 'hours'} ago`;
-  
-    default:
-      return formatDate(dateString);
-  }
-}
 export default {
   getCurrentDate,
   getDueDate,
-  diffTime,
-  convertDateInput,
-  formatDate,
-  timeElapse,
+  diffTime
 };

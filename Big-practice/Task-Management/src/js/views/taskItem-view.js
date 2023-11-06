fd736c7ed614f80d97c227c54ce23289bc799d03
tranlekhanhduyen <trankhanhduyen312@gmail.com>
2023-11-06 08:19:41 +0700

    feat: create task detail

--- Big-practice/Task-Management/src/js/views/taskItem-view.js
+++ Big-practice/Task-Management/src/js/views/taskItem-view.js
@@ -1,10 +1,13 @@
 // import TaskItemTemplate from "../template/taskItem-template";
-// import TaskDetailTemplate from "../template/task-detail";
+import TaskDetailTemplate from "../template/taskDetail-template";
 // import TaskModel from "../models/task";
 import iconDelete from "../../assets/icons/delete.svg";
 import iconClock from "../../assets/icons/clock.svg";
 import date from "../utilities/date";
 
+// import iconEdit from "../../assets/icons/edit.svg";
+// import iconClose from "../../assets/icons/close.svg";
+// import userAvatar from "../../assets/images/user.svg";
 import TaskListModel from "../models/taskList-model";
 
 export default class TaskItemView {
@@ -13,7 +16,7 @@ export default class TaskItemView {
     this.taskInput = document.querySelector(".task-input");
     this.todoBoard = document.getElementById("js-todo");
     this.taskListModel = new TaskListModel();
-    // this.taskItem = document.querySelector(".task-item-container");
+    // this.taskItems = document.querySelectorAll(".task-item-container");
     this.bindAddTask(this.showTaskItem);
 
     // Get tasks from API
@@ -28,7 +31,7 @@ export default class TaskItemView {
     console.log(this.tasks);
     // TODO: Refactor later
     // Get task list area
-    const taskListDisplay = document.getElementById("task-list");
+    const taskListDisplay = document.querySelector(".task-list");
     console.log(taskListDisplay);
     taskListDisplay.innerHTML = "";
 
@@ -46,7 +49,7 @@ export default class TaskItemView {
         <p class="date-ago">${date.diffTime(data.createdDate)} days ago</p>
         <div class="date-left text-sm">
           <img src="${iconClock}" class="clock-icon">
-          <p>${date.diffTime(data.dueDate, Math.ceil, "left")}</p>
+          <p>${date.diffTime(data.dueDate, Math.ceil, "days left")}</p>
         </div>
       </div>
     </li>
@@ -70,8 +73,8 @@ export default class TaskItemView {
           const addedTask = await this.taskListModel.addTask(newTaskName);
 
           // Update the current task list
-          // this.tasks = [...this.tasks, addedTask];
-          this.tasks.push(addedTask);
+          this.tasks = [...this.tasks, addedTask];
+          // this.tasks.push(addedTask);
 
           // Show the tasks
           this.showTaskItem();
@@ -82,6 +85,73 @@ export default class TaskItemView {
           console.log("task is empty");
         }
       }
+    });
+  }
+  //  taskItems.forEach(taskItem => {
+
+  //  })
+
+  // showTaskDetail() {
+  //   const taskItems = document.querySelectorAll(".task-item-container");
+  //   taskItems.innerHTML = " ";
+  //   taskItems.forEach((taskItem) => {
+  //     taskItem.addEventListener("click", () => {
+  //       // Show the task detail page
+  //       this.showTaskDetail(taskItem);
+  //     });
+  //   });
+  // }
+
+  // showTaskDetails() {
+  //   const taskItems = document.querySelectorAll(".task-item-container");
+  //   console.log(taskItems, "bkgs");
+  //   taskItems.forEach((taskItem) => {
+  //     taskItem.addEventListener("click", () => {
+  //       // Show the task detail page
+  //       console.log("task detail", taskItem);
+  //       this.showTaskDetail(taskItem);
+  //     });
+  //   });
+  // }
+  // showTaskDetail(taskItem) {
+  //   // Get the task data
+  //   const taskData = taskItem.dataset.task;
+
+  //   // Render the task detail
+  //   const taskDetail = TaskDetailTemplate.renderTaskDetail(taskData);
+
+  //   // Add the task detail to the DOM
+  //   const taskDetailContainer = document.querySelector(
+  //     ".detail-task-container"
+  //   );
+  //   taskDetailContainer.innerHTML = taskDetail;
+
+  //   // Show the task detail container
+  //   taskDetailContainer.style.display = "block";
+  // }
+
+  showTaskDetail() {
+    const taskDetailContainer = document.querySelector(
+      ".detail-task-container"
+    );
+    const closeIcon = document.querySelector(".close-icon");
+
+    const taskItems = document.querySelectorAll(".task-item");
+
+    taskItems.forEach((taskItem) => {
+      console.log(taskItem, "abc");
+      taskItem.addEventListener("click", () => {
+        const task = taskItem.dataset.task;
+        console.log(task, "sh");
+        taskDetailContainer.innerHTML =
+          TaskDetailTemplate.renderTaskDetail(task);
+        taskDetailContainer.style.display = "block";
+        console.log(taskDetailContainer, "task detail");
+      });
+    });
+
+    closeIcon.addEventListener("click", () => {
+      taskDetailContainer.style.display = "none";
     });
   }
 }

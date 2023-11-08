86b057a0096881a75f0d28cee7137fe8a11c2d4a
Duyen <trankhanhduyen312@gmail.com>
2023-11-08 16:56:53 +0700

    fix: recreate service

---
+++ Big-practice/Task-Management/src/js/services/task.js
@@ -0,0 +1,27 @@
+import { API_TASKS } from "../constants/url";
+import APIHelper from "./helper";
+
+export default class APITask {
+  async addTask(taskName) {
+    const response = await fetch(
+      API_TASKS,
+      APIHelper.requestOptions("POST", taskName)
+    );
+    const result = await response.json();
+
+    return {
+      status: response.status,
+      data: result,
+    };
+  }
+
+  async get() {
+    const response = await fetch(API_TASKS);
+    const result = await response.json();
+
+    return {
+      status: response.status,
+      data: result,
+    };
+  }
+}

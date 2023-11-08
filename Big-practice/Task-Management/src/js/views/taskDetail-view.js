ead50f0097dafdd17c07bec6231bf18d6052fc50
Duyen <trankhanhduyen312@gmail.com>
2023-11-08 09:14:34 +0700

    feat: create task detail

---
+++ Big-practice/Task-Management/src/js/views/taskDetail-view.js
@@ -0,0 +1,10 @@
+import TaskItemTemplate from "../template/taskItem-template";
+
+export default class TaskDetailView {
+  constructor() {
+    this.taskItem = document.querySelector(".task-item-container");
+    this.updateTasks = [];
+  }
+
+  
+}

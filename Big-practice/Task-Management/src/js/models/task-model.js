58c2e831de6bef9b15a5999a84d6fc5d147dc993
Duyen <trankhanhduyen312@gmail.com>
2023-12-07 13:54:30 +0700

    feat: change class

--- Big-practice/Task-Management/src/js/models/task-model.js
+++ Big-practice/Task-Management/src/js/models/task-model.js
@@ -1,13 +1,13 @@
 import date from "../utilities/date";
 import STATUS from "../constants/status";
 
-const TaskModel = (taskName) => ({
-  taskName,
-  description: "",
-  dueDate: date.getDueDate(),
-  createdDate: date.getCurrentDate(),
-  comments: "",
-  status: STATUS.TODO,
-});
-
-export default TaskModel;
+export default class TaskModel {
+  constructor(taskName) {
+    this.taskName = taskName;
+    this.description = "";
+    this.dueDate = date.getDueDate();
+    this.createdDate = date.getCurrentDate();
+    this.comments = "";
+    this.status = STATUS.TODO;
+  }
+}

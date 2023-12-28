764681ebba7faeade9d2166c40df2310fbb03469
Duyen <trankhanhduyen312@gmail.com>
2023-12-28 16:43:58 +0700

    feat: example about boolean type

---
+++ example/ex10-boolean-type/example10.ts
@@ -0,0 +1,7 @@
+let status1: boolean = false;
+let check1: boolean = true;
+let pro: boolean = Boolean("");
+let pro1: boolean = Boolean("hi");
+
+console.log(">>> check pro: ", pro); //false
+console.log(">>> check pro1: ", pro1); //true

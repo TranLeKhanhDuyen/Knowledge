bc374c6d2470119f6f25eccae6ba2615cec6ee41
Duyen <trankhanhduyen312@gmail.com>
2023-12-28 16:17:38 +0700

    feat: example about type interface

---
+++ example/ex7-type-inference/example7.js
@@ -0,0 +1,5 @@
+let count = 10;
+
+count = 'duyen';
+
+console.log(">>> check count = ", count)
---
+++ example/ex7-type-inference/example7.ts
@@ -0,0 +1,9 @@
+let count: string = "Duyen Tran";
+
+console.log(">>> check count = ", count);
+
+let test = ["duyen", "tran", "typescirpt", 123]; //array
+
+test.push(25);
+
+console.log(">>> check test = ", test);

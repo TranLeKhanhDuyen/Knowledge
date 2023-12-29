b6b5be5b39eddf95c58671872755d045122a38a3
Duyen <trankhanhduyen312@gmail.com>
2023-12-29 10:47:03 +0700

    feat: add example about void type

---
+++ example/ex16-void-type/lesson16.ts
@@ -0,0 +1,7 @@
+const sum = (a: number, b: number): number => {
+  return a + b;
+};
+
+const handeLogs = (message: string): void => { //void: not return value
+    console.log(">>> message: ", message)
+}

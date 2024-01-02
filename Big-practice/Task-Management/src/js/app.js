4af8f77f8da5dd2c35c904887e90a3b9668c9563
tranlekhanhduyen <trankhanhduyen312@gmail.com>
2024-01-02 12:55:04 +0700

    feat: change comment list model with typescript

--- Big-practice/Task-Management/src/js/app.js
+++ Big-practice/Task-Management/src/js/app.js
@@ -2,7 +2,7 @@ import Controller from './controller/task-controller.js';
 import TaskListModel from './models/taskList-model.js';
 import TaskListView from './views/taskList-view.js';
 import TaskDetailView from './views/taskDetail-view.js';
-import CommentListModel from './models/commentList-model.js';
+import CommentListModel from './models/commentList-model.ts';
 
 new Controller(
   new TaskListModel(),

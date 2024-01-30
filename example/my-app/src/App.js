61b65107d03e7a48deeee8dae150f69abf97b168
Duyen <trankhanhduyen312@gmail.com>
2024-01-30 11:38:17 +0700

    feat: example fix stuck form inputs

--- example/my-app/src/App.js
+++ example/my-app/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import "./components/style.css";
 import Gallery from "./components/Props/Profile";
 import State from "./components/Sate-variable/state";
+import Form from "./components/Sate-variable/form";
 
 function App() {
   return (
     <div>
       <Gallery />
       <State />
+      <Form />
     </div>
   );
 }

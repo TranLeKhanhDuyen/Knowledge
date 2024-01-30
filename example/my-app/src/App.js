95fe59da679439b3b7c15511de0a59f828f26dbf
Duyen <trankhanhduyen312@gmail.com>
2024-01-30 09:45:56 +0700

    feat: state variable

--- example/my-app/src/App.js
+++ example/my-app/src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
 import "./components/style.css";
 import Gallery from "./components/Props/Profile";
+import State from "./components/Sate-variable/state";
 
 function App() {
-  return <Gallery />;
+  return (
+    <div>
+      <Gallery />
+      <State />
+    </div>
+  );
 }
 
 export default App;

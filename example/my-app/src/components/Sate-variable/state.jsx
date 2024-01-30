ba1906512ed40565f51635813e697c6b1f506b44
Duyen <trankhanhduyen312@gmail.com>
2024-01-30 11:07:43 +0700

    feat: state variable

--- example/my-app/src/components/Sate-variable/state.jsx
+++ example/my-app/src/components/Sate-variable/state.jsx
@@ -3,23 +3,32 @@ import { sculptureList } from "./data";
 
 export default function State() {
   const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
 
-  function handleClick() {
+  const handleNextClick = () => {
     setIndex(index + 1);
-  }
+  };
+
+  const handleMoreClick = () => {
+    setShowMore(!showMore);
+  };
 
   let sculpture = sculptureList[index];
   return (
     <>
-      <button onClick={handleClick}>Next</button>
+      <button onClick={handleNextClick}>Next</button>
       <h2>
         <i>{sculpture.name}</i> by {sculpture.artist}
       </h2>
       <h3>
         ({index + 1} of {sculptureList.length})
       </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? "Hide" : "Show"} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+
       <img src={sculpture.url} alt={sculpture.alt} />
-      <p>{sculpture.description}</p>
     </>
   );
 }

9343096965a8b4a434714cd1c853681c25986d02
Duyen <trankhanhduyen312@gmail.com>
2024-02-02 10:36:50 +0700

    feat: add router and route

--- example/my-app/src/App.jsx
+++ example/my-app/src/App.jsx
@@ -14,23 +14,78 @@ import FormSate from "./components/UseSate/Form";
 import ChatRoomApp from "./components/UseEffect/ChatRoom";
 import Todo from "./components/Todo/Todo";
 
+import { Routes, Route, Link, BrowserRouter as Router } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-      <Gallery />
-      <State />
-      <Form />
-      <FeedbackForm />
-      <VideoPlayerApp />
-      <MyInputForm />
-      <PackingList />
-      <DrinkList />
-      <Counter />
-      <MyInput />
-      <FormSate />
-      <ChatRoomApp />
-      <Todo />
-    </div>
+    <>
+      <Router>
+        <>
+          <Link to="/props">
+            <p>Props</p>
+          </Link>
+          <Link to="/state">
+            <p>State</p>
+          </Link>
+          <Link to="/effect">
+            <p>Effect</p>
+          </Link>
+          <Link to="/render">
+            <p>Render</p>
+          </Link>
+          <Link to="/usestate">
+            <p>UseSate</p>
+          </Link>
+          <Link to="/useeffect">
+            <p>UseEffect</p>
+          </Link>
+        </>
+
+        <Routes>
+          <Route path="/props" element={<Gallery />} />
+          <Route
+            path="/state"
+            element={
+              <>
+                <State />
+                <Form />
+                <FeedbackForm />
+              </>
+            }
+          />
+          <Route
+            path="/effect"
+            element={
+              <>
+                <VideoPlayerApp />
+                <MyInputForm />
+              </>
+            }
+          />
+          <Route
+            path="/render"
+            element={
+              <>
+                <PackingList />
+                <DrinkList />
+              </>
+            }
+          />
+          <Route
+            path="/usestate"
+            element={
+              <>
+                <Counter />
+                <MyInput />
+                <FormSate />
+              </>
+            }
+          />
+          <Route path="/useeffect" element={<ChatRoomApp />} />
+          <Route path="/todo" element={<Todo />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 

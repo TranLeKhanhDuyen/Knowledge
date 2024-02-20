1f995a2623020588b2c38e7dfe058f06c063cc74
Duyen <trankhanhduyen312@gmail.com>
2024-02-20 15:36:35 +0700

    feat: update header

--- BookingCare/src/containers/Header/Header.jsx
+++ BookingCare/src/containers/Header/Header.jsx
@@ -1,41 +1,41 @@
 import React from 'react';
-import '../../styles/sections/header.css';
+import './header.css';
 
-export default class Header {
-  render() {
-    return (
-      <header className='header-container'>
-        <div className='header-content'>
-          <div className='left-content'>
-            <i className='fas fa-bars'></i>
-            <div className='header-logo'></div>
-          </div>
-
-          <nav className='center-content'>
-            <div className='child-content'>
-              <b>Specialist</b>
-              <p className='subs-title'>Find doctors by specialty</p>
-            </div>
-            <div className='child-content'>
-              <b>Health facilities</b>
-              <p className='subs-title'>Choose a hospital or clinic</p>
-            </div>
-            <div className='child-content'>
-              <b>Doctor</b>
-              <p className='subs-title'>Choose a doctor</p>
-            </div>
-            <div className='child-content'>
-              <b>Examination package</b>
-              <p className='subs-title'>General examination</p>
-            </div>
-          </nav>
-
-          <div className='right-content'>
-            <i className='fa-regular fa-circle-question'></i>
-            <p>Support</p>
-          </div>
+const Header = () => {
+  return (
+    <header className='header-container'>
+      <div className='header-content'>
+        <div className='left-content'>
+          <i className='fas fa-bars'></i>
+          <div className='header-logo'></div>
         </div>
-      </header>
-    );
-  }
-}
+
+        <nav className='center-content'>
+          <div className='child-content'>
+            <b>Specialist</b>
+            <p className='subs-title'>Find doctors by specialty</p>
+          </div>
+          <div className='child-content'>
+            <b>Health facilities</b>
+            <p className='subs-title'>Choose a hospital or clinic</p>
+          </div>
+          <div className='child-content'>
+            <b>Doctor</b>
+            <p className='subs-title'>Choose a doctor</p>
+          </div>
+          <div className='child-content'>
+            <b>Examination package</b>
+            <p className='subs-title'>General examination</p>
+          </div>
+        </nav>
+
+        <div className='right-content'>
+          <i className='fa-regular fa-circle-question'></i>
+          <p>Support</p>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

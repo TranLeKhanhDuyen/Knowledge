c5273ab3ef22262260a41d428eb677d50674b90a
Duyen <trankhanhduyen312@gmail.com>
2024-02-21 17:52:03 +0700

    feat: update components

---
+++ BookingCare/src/components/PageLinkSecondary/PageLink.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import specialist from '../../assets/icons/ic-specialist.png';
+import remote from '../../assets/icons/ic-remote.png';
+import dental from '../../assets/icons/ic-dental.png';
+import general from '../../assets/icons/ic-general.png';
+import medicalTest from '../../assets/icons/ic-medical-tests.png';
+import mentealHeath from '../../assets/icons/ic-mental-health.png';
+
+import PageLink from '.';
+
+export default {
+  title: 'Components/Button',
+  component: PageLink
+} as Meta;
+
+type Story = StoryObj<typeof PageLink>;
+
+export const Default: Story = {
+  args: {
+    items: [
+      {
+        imagePath: specialist,
+        title: 'Specialist Examnination'
+      },
+      {
+        imagePath: remote,
+        title: 'Remote Examnination'
+      },
+      {
+        imagePath: dental,
+        title: 'Dental Examnination'
+      },
+      {
+        imagePath: general,
+        title: 'General Examnination'
+      },
+      {
+        imagePath: medicalTest,
+        title: 'Medical tests'
+      },
+      {
+        imagePath: mentealHeath,
+        title: 'Mental heath'
+      }
+    ]
+  }
+};

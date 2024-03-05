adec1d7209591fb7efbd73778f3589ae57c3ec89
Duyen <trankhanhduyen312@gmail.com>
2024-03-05 14:24:59 +0700

    feat: show hide password

--- BookingCare/src/containers/Auth/Login.tsx
+++ BookingCare/src/containers/Auth/Login.tsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import './Login.css';
 
+interface ILoginState {
+  username: string;
+  password: string;
+  isShowPassword: boolean;
+}
+
 const Login = () => {
+  const [state, setState] = useState<ILoginState>({
+    username: 'Duyen tran',
+    password: '11111',
+    isShowPassword: false
+  });
+
+  const handleOnChangeUsername = (event: ChangeEvent<HTMLInputElement>) => {
+    setState({
+      ...state,
+      username: event.target.value
+    });
+  };
+
+  const handleOnChangePassword = (event: ChangeEvent<HTMLInputElement>) => {
+    setState({
+      ...state,
+      password: event.target.value
+    });
+  };
+
+  const handleLogin = () => {
+    console.log('username: ', state.password);
+    console.log('password: ', state.password);
+    console.log('all state: ', state);
+  };
+
+  const handleShowHidePassword = () => {
+    setState({
+      ...state,
+      isShowPassword: !state.isShowPassword
+    });
+  };
+
   return (
     <>
       <div className='login-background'>
@@ -14,22 +53,37 @@ const Login = () => {
                 type='text'
                 className='username text-field'
                 placeholder='Enter your username'
+                value={state.username}
+                onChange={(event) => handleOnChangeUsername(event)}
               />
-            </div>
-            <div className='form-input'>
               <label>Password</label>
-              <input
-                type='text'
-                className='password text-field'
-                placeholder='Enter your password'
-              />
+              <div className='password-container'>
+                <input
+                  className='password text-field'
+                  type={state.isShowPassword ? 'text' : 'password'}
+                  placeholder='Enter your password'
+                  value={state.password}
+                  onChange={(event) => handleOnChangePassword(event)}
+                />
+                <span
+                  className={`eye cursor ${
+                    state.isShowPassword ? 'hide-eye' : 'show-eye'
+                  }`}
+                  onClick={handleShowHidePassword}
+                ></span>
+              </div>
             </div>
-            <button className='login-button text-bold'>Login</button>
+            <button
+              className='login-button text-bold cursor'
+              onClick={handleLogin}
+            >
+              Login
+            </button>
             <span className='forgot-password'>Forgot your password?</span>
-            <span className='option-text'> Or Login with: </span>
+            <span className='option-text'>Or Login with: </span>
             <div className='afternative-login'>
-              <a href='#' className='google'></a>
-              <a href='#' className='facebook'></a>
+              <a href='#' className='login-google'></a>
+              <a href='#' className='login-facebook'></a>
             </div>
           </div>
         </div>

f49f6d2b031155ca4d142ff8b709a11598135d47
tranlekhanhduyen <trankhanhduyen312@gmail.com>
2024-03-25 22:48:58 +0700

    feat: refactor code

--- BookingCare/src/components/Slider/Slider.tsx
+++ BookingCare/src/components/Slider/Slider.tsx
@@ -1,19 +1,24 @@
-import ItemLink, { IItemLinkProps, TItemLinkType } from '@components/ItemLink/ItemLink';
-import './Slider.css'
-import { useEffect, useState } from 'react';
-import Slider from 'react-slick';
+import { useEffect, useState } from 'react'
+import Slider from 'react-slick'
 // Import css files
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { TImage } from '@components/common/Image/Image';
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import ItemLink, {
+  IItemLinkProps,
+  TItemLinkType
+} from '@components/ItemLink/ItemLink'
+import { TImageVariant } from '@components/common/Image/Image'
+import './Slider.css'
 
 interface ISliderProps {
-  items: IItemLinkProps[];
-  types: TItemLinkType,
-  typeImage?: TImage
+  items: IItemLinkProps[]
+  types: TItemLinkType
+  typeImage?: TImageVariant
+  width?: string
+  height?: string
 }
 
-const CustomSlider = ({ types, items, typeImage }: ISliderProps) => {
+const CustomSlider = ({ types, items, typeImage, width, height }: ISliderProps) => {
   const settings = {
     dots: false,
     infinite: true,
@@ -22,50 +27,42 @@ const CustomSlider = ({ types, items, typeImage }: ISliderProps) => {
     slidesToScroll: 4
   }
 
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [currentIndex, setCurrentIndex] = useState<number>(0)
 
   useEffect(() => {
     if (types === 'banner') {
       const interval = setInterval(() => {
-        setCurrentIndex(prevIndex =>
+        setCurrentIndex((prevIndex) =>
           prevIndex === items.length - 1 ? 0 : prevIndex + 1
-        );
-      }, 3000);
-      return () => clearInterval(interval);
+        )
+      }, 3000)
+      return () => clearInterval(interval)
     }
-  }, [types, items, typeImage]);
+  }, [types, items, typeImage])
 
   const renderSliderItems = () => {
     if (types === 'banner') {
       return (
-        <div className="slider-items">
+        <div className='slider-items'>
           <ItemLink {...items[currentIndex]} />
         </div>
-      );
+      )
     } else {
       return (
-        <Slider  {...settings}>
+        <Slider {...settings} className={`slider slider-${types}`}>
           {items.map((item) => (
             <ItemLink
-              type={item.type}
-              image={item.image}
-              typeImage={typeImage}
-              title={item.title}
-              subsTitle={item.subsTitle}
-              description={item.description}
-              onClick={item.onClick}
+              {...item}
+              width={width}
+              height={height}
             />
           ))}
         </Slider>
-      );
+      )
     }
-  };
+  }
 
-  return (
-    <div className="container slider-container">
-      {renderSliderItems()}
-    </div>
-  );
-};
+  return <div className='container slider-container'>{renderSliderItems()}</div>
+}
 
-export default CustomSlider;
+export default CustomSlider

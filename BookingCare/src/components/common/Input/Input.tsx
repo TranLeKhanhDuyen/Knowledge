9d7438c4cbbc5c5bba1cd8d9927df0e174ef2edb
tranlekhanhduyen <trankhanhduyen312@gmail.com>
2024-03-25 22:45:37 +0700

    feat: update input common

--- BookingCare/src/components/common/Input/Input.tsx
+++ BookingCare/src/components/common/Input/Input.tsx
@@ -1,33 +1,22 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent } from 'react'
 import './Input.css'
 
-export interface IInputProps {
-  variant?: 'search' | 'text';
-  type: 'text'| 'checkbox'|'email'|'password'|'date';
-  placeholder?: string;
-  name?: string;
-  value?: string;
-  onChange: (value: string) => void;
+export interface IInputProps extends React.HTMLProps<HTMLInputElement> {
+  additionalClass: string
+  onChangeValue: (value: string) => void
   required?: boolean
 }
 
-const Input = ({
-  variant ,
-  placeholder,
-  name,
-  value,
-  onChange
-}: IInputProps) => {
+const Input = ({ additionalClass, onChangeValue, ...props }: IInputProps) => {
   const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
-    onChange(event.target.value)
-  };
+    onChangeValue(event.target.value)
+  }
+
   return (
     <input
-      className={`input-${variant}`}
-      placeholder={placeholder}
-      name={name}
-      value={value}
+      className={`input-${additionalClass}`}
       onChange={handleOnChange}
+      {...props}
     />
   )
 }

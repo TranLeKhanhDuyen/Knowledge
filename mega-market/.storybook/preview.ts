5810e5b2ed406f6a2b20ca2781b65a716b3cf49c
tranlekhanhduyen <trankhanhduyen312@gmail.com>
2024-04-22 14:17:37 +0700

    feat: create components

--- mega-market/.storybook/preview.ts
+++ mega-market/.storybook/preview.ts
@@ -1,9 +1,8 @@
-// import type { Preview } from "@storybook/react";
-import '../src/styles/main.css'
+// import type { Preview } from '@storybook/react'
+import '../src/styles/main.css';
 
 const preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,

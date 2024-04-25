5b4a75dd3be71df0ae07223c3f354927a17380d2
Duyen <trankhanhduyen312@gmail.com>
2024-04-25 16:51:01 +0700

    feat: create topbar

--- mega-market/src/App.tsx
+++ mega-market/src/App.tsx
@@ -1,7 +1,12 @@
-import './App.css'
+import HomePage from '@pages/home'
+import './styles/main.css'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='app'>
+      <HomePage />
+    </div>
+  )
 }
 
 export default App

991aafb40d842e85c2e06f7b0935587d364de996
Duyen <trankhanhduyen312@gmail.com>
2024-05-15 17:16:10 +0700

    update: setup and config files

--- unit-test/tests/components/Greet.test.tsx
+++ unit-test/tests/components/Greet.test.tsx
@@ -1,6 +1,4 @@
-import { render, screen } from "@testing-library/react";
-import { it, describe, expect } from "vitest";
-import "@testing-library/jest-dom/vitest";
+import { render, screen } from '@testing-library/react'
 import Greet from "../../src/components/Greet";
 
 describe("Greet", () => {

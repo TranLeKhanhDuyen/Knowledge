b00ceca2e5e56ae354847833ded99293044780ab
Duyen <trankhanhduyen312@gmail.com>
2024-05-21 10:55:22 +0700

    feat: update url set up test

--- mega-market/jest.config.cjs
+++ mega-market/jest.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/src/test/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
     '\\.(css|less|scss|sss|styl)$': 'identity-obj-proxy'
   }

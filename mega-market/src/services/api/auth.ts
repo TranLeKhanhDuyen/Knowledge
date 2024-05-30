f50e1601527d8a65f7006d56291c0a3527d339fd
Duyen <trankhanhduyen312@gmail.com>
2024-05-30 09:21:38 +0700

    feat: refactor code

--- mega-market/src/services/api/auth.ts
+++ mega-market/src/services/api/auth.ts
@@ -1,30 +1,11 @@
 import { User } from '@services/models/user'
 import { RegisterRequest, SignInRequest } from '@services/request/authRequest'
+import { axiosRequest } from './helper';
 
 export const registerUser = async (data: RegisterRequest): Promise<User> => {
-  const response = await fetch('/api/register', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data)
-  })
-
-  if (!response.ok) {
-    throw new Error('Register failed')
-  }
-
-  return response.json()
+  return axiosRequest<User>('/api/register', 'POST', data);
 }
 
 export const loginUser = async (data: SignInRequest): Promise<User> => {
-  const response = await fetch('/api/login', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data)
-  })
-
-  if (!response.ok) {
-    throw new Error('Login failed')
-  }
-
-  return response.json() 
+  return axiosRequest<User>('/api/login', 'POST', data);
 }
--- mega-market/src/services/models/cart.ts
+++ mega-market/src/services/models/cart.ts
@@ -1,10 +1,10 @@
 export interface CartItem {
-  id: number;
-  primaryImage: string;
-  name: string;
-  regular_price: number;
-  discount: number;
-  quantity: number;
+  id: number
+  primaryImage: string
+  name: string
+  regular_price: number
+  discount: number
+  quantity: number
   price: number
   isSelect: boolean
 }
